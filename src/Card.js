12b442861edf2e66cac43c59c401a728f2623288
talon152 <talon152@gmail.com>
2018-11-20 22:21:19 -0500

    cards

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+	<div>
+		<img alt="robots" src=""/>
+		<div>
+		<h2>Jane Doe<h2>
+		<p>Jane.Doe@gmail.com>/p>
+		</div>
+	</div>
+		);
+
+}
+
+export default Card;
