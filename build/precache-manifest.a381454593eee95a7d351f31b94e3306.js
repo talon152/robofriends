3de3dceba03c68db6d2610a7a30739eb2484d182
talon152 <45188472+talon152@users.noreply.github.com>
2018-11-25 17:19:17 -0500

    Add files via upload

---
+++ build/precache-manifest.a381454593eee95a7d351f31b94e3306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "edf48188b13abbb8fdee",
+    "url": "/static/js/main.edf48188.chunk.js"
+  },
+  {
+    "revision": "2e733375b7da31e56d67",
+    "url": "/static/js/1.2e733375.chunk.js"
+  },
+  {
+    "revision": "edf48188b13abbb8fdee",
+    "url": "/static/css/main.2f265d9e.chunk.css"
+  },
+  {
+    "revision": "2e733375b7da31e56d67",
+    "url": "/static/css/1.1d030298.chunk.css"
+  },
+  {
+    "revision": "e0144bf8adff9fecec3c35a7b6d82252",
+    "url": "/index.html"
+  }
+];
